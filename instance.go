4b3942eaeab789a8395092c6bd0d6760f76dac4e
henkhuang <987337272@qq.com>
2020-04-21 20:18:44 +0800

    add files

---
+++ instance.go
@@ -0,0 +1,32 @@
+package balance
+
+import "strconv"
+
+type Instance struct {
+	Host      string
+	Port      int64
+	CallTimes int64
+}
+
+func NewInstance(host string, port int64) *Instance {
+	return &Instance{
+		Host: host,
+		Port: port,
+	}
+}
+
+func (i *Instance) GetPort() int64 {
+	return i.Port
+}
+
+func (i *Instance) GetHost() string {
+	return i.Host
+}
+
+func (i *Instance) GetCallTimes() string {
+	return i.CallTimes
+}
+
+func (i *Instance) GetUrl() string {
+	return i.Port + ":" + strconv.Itoa(i.Port)
+}
